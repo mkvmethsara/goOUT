121f8d58179ab995e38c4437acb786c20f0e1ce3
mkvmethsara <mkvmethsara@students.nsbm.ac.lk>
2026-06-10 08:32:49 +0530

    Initial clean Spring Boot with MongoDB setup

---
+++ src/main/java/com/squadx/goout/GoOutApplication.java
@@ -0,0 +1,13 @@
+package com.squadx.goout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/squadx/goout/GoOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadx.goout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
