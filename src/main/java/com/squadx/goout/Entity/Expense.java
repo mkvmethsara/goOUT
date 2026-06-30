ae8c5ead8f336597146e9d5ad346645bc0718ca0
dewnaka paranagama <imaldewnaka@gmail.com>
2026-06-30 22:11:38 +0530

    feat: create expense Entity

--- src/main/java/com/squadx/goout/Entity/Expense.java
+++ src/main/java/com/squadx/goout/Entity/Expense.java
@@ -6,30 +6,27 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "expenses")
-
 public class Expense {
 
     @Id
     private String id;
 
-    // The ID of the specific trip this expense belongs to
     private String tripId;
-
-    // The ID of the user who actually paid the bill
-    private String paidByUserId;
-
-    private String description;
+    private String title;
     private double amount;
-
-
-
     private String category;
 
-    private LocalDate date = LocalDate.now();
+    // The display name the frontend sends for the MVP (e.g., "Vishwa")
+    private String paidBy;
+
+    // The actual database ID of the user (We will need this later for the Math Engine!)
+    private String userId;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
