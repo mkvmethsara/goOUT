9d4dab44f0261763f93cbcb860e0f62a96bb6b0d
Udaiimesh <udaiimeshedu49@gmail.com>
2026-07-06 20:38:06 +0530

    Create Like Dto

---
+++ src/main/java/com/squadx/goout/Dto/LikeResponseDto.java
@@ -0,0 +1,14 @@
+package com.squadx.goout.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeResponseDto {
+    private boolean success;
+    private boolean isLikedByCurrentUser;
+    private int likeCount;
+}
