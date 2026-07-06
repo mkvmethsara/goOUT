336d2a0cfedf70e51a0d3dfcec573404a1b62db7
dewnaka paranagama <imaldewnaka@gmail.com>
2026-07-06 10:19:47 +0530

    Create Change Password Dto

---
+++ src/main/java/com/squadx/goout/Dto/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.squadx.goout.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+}
